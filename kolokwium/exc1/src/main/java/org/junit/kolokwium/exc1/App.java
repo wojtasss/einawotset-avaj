ee4fd6feb289dd854e60481cf8d52e20a793e4bb
wojtasss <wojcieh.rzepecki@gmail.com>
2016-03-02 16:05:59 +0100

    Create project

---
+++ kolokwium/exc1/src/main/java/org/junit/kolokwium/exc1/App.java
@@ -0,0 +1,13 @@
+package org.junit.kolokwium.exc1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

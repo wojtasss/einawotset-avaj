1b49bd84548bc2bf7dfdba9c1c53e08e0fdec469
wojtasss <wojcieh.rzepecki@gmail.com>
2016-03-02 21:35:50 +0100

    Exception JUnit Test Case for top() method.

---
+++ kolokwium/exc1/src/test/java/org/junit/kolokwium/exc1/tests/StackTopExceptionTest.java
@@ -0,0 +1,47 @@
+package org.junit.kolokwium.exc1.tests;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.kolokwium.exc1.Stack;
+import org.junit.kolokwium.exc1.exceptions.EmptyStackException;
+import org.junit.kolokwium.exc1.exceptions.NullStackException;
+
+public class StackTopExceptionTest {
+	
+	private Stack stack;
+	
+	@Before
+	public void setUp() throws Exception {
+		stack = new Stack();
+	}
+	
+	@Test
+	public void NullStackExceptionTest() {
+		stack.setStack(null);
+		try {
+			stack.top();
+		} catch (NullStackException ex) {
+			assertTrue(true);
+		}
+	}
+	
+	@Test
+	public void EmptyStackExceptionTest() {
+		stack.setStack(new ArrayList<Integer>());
+		try {
+			stack.top();
+		} catch (EmptyStackException ex) {
+			assertTrue(true);
+		}
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		stack = null;
+	}
+}

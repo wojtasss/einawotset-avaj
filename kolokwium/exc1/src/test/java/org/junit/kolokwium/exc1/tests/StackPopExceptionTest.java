b76c1becd17b3b7f0f2df7e9f8931b93992fc1a2
wojtasss <wojcieh.rzepecki@gmail.com>
2016-03-02 20:59:04 +0100

    Exception JUnit Test Case for pop() method.

---
+++ kolokwium/exc1/src/test/java/org/junit/kolokwium/exc1/tests/StackPopExceptionTest.java
@@ -0,0 +1,46 @@
+package org.junit.kolokwium.exc1.tests;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.kolokwium.exc1.Stack;
+import org.junit.kolokwium.exc1.exceptions.NullStackException;
+
+public class StackPopExceptionTest {
+	
+	private Stack stack;
+	
+	@Before
+	public void setUp() throws Exception {
+		stack = new Stack();
+	}
+	
+	@Test
+	public void NullStackExceptionTest() {
+		stack.setStack(null);
+		try {
+			stack.pop();
+		} catch (NullStackException ex) {
+			assertTrue(true);
+		}
+	}
+	
+	@Test
+	public void EmptyStackExceptionTest() {
+		stack.setStack(new ArrayList<Integer>());
+		try {
+			stack.pop();
+		} catch (NullStackException ex) {
+			assertTrue(true);
+		}
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		stack = null;
+	}
+}

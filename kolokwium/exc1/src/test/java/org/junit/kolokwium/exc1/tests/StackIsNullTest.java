ea86f60f5f8ccf37632add97eb262662a799b453
wojtasss <wojcieh.rzepecki@gmail.com>
2016-03-02 21:07:43 +0100

    JUnit Test Case for isNull() method.

---
+++ kolokwium/exc1/src/test/java/org/junit/kolokwium/exc1/tests/StackIsNullTest.java
@@ -0,0 +1,39 @@
+package org.junit.kolokwium.exc1.tests;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.kolokwium.exc1.Stack;
+
+public class StackIsNullTest {
+	
+	private Stack stack;
+	
+	@Before
+	public void setUp() throws Exception {
+		stack = new Stack();
+	}
+	
+	@Test
+	public void StackIsNullTest() {
+		stack.setStack(null);
+		boolean result = stack.isNull();
+		assertTrue(result);
+	}
+	
+	@Test
+	public void StackIsNotNullTest() {
+		stack.setStack(new ArrayList<Integer>());
+		boolean result = stack.isNull();
+		assertFalse(result);
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		stack = null;
+	}
+}

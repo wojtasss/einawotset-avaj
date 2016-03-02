85c296168e8ff53bb9a46261d9aec11f40b01381
wojtasss <wojcieh.rzepecki@gmail.com>
2016-03-02 20:54:17 +0100

    Correct JUnit Test Case for pop() method.

---
+++ kolokwium/exc1/src/test/java/org/junit/kolokwium/exc1/tests/StackPopCorrectTests.java
@@ -0,0 +1,53 @@
+package org.junit.kolokwium.exc1.tests;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.kolokwium.exc1.Stack;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class StackPopCorrectTests {
+	
+	private Stack stack;
+	private ArrayList<Integer> data;
+	private int expected;
+	
+	public StackPopCorrectTests(ArrayList<Integer> data, int expected) {
+		this.data = data;
+		this.expected = expected;
+	}
+	
+	@Parameterized.Parameters
+    public static Iterable<Object[]> data() {
+        return Arrays.asList(
+                new Object[][]{ 
+                	{new ArrayList<Integer>() {{add(0, 1); }}, 1},
+                	{new ArrayList<Integer>() {{add(0, 1); add(0, 2); }}, 2},
+                	{new ArrayList<Integer>() {{add(0, 1); add(0, 2); add(0, 3); }}, 3}
+                });
+    }
+	
+	@Before
+	public void setUp() throws Exception {
+		stack = new Stack();
+	}
+
+	@Test
+	public void test() {
+		stack.setStack(data);
+		int result = stack.pop();
+		assertEquals(expected, result);
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		stack = null;
+	}
+}

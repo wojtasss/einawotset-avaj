4d1d00c7476dbf84a554b6b8aa87d28befa98794
wojtasss <wojcieh.rzepecki@gmail.com>
2016-03-03 01:14:25 +0100

    Create interface Psikus.

---
+++ exc2/src/main/java/org/junit/kolokwium/exc2/interfaces/Psikus.java
@@ -0,0 +1,11 @@
+package org.junit.kolokwium.exc2.interfaces;
+
+import org.junit.kolokwium.exc2.exception.NieudanyPsikusExcpetion;
+
+public interface Psikus {
+	public Integer CyfroKrad(Integer liczba);
+	public Integer HultajChochla(Integer liczba) throws NieudanyPsikusExcpetion;
+	public Integer Nieksztalek(Integer liczba);
+	public Integer Heheszki(Integer liczba);
+	boolean Titit(Integer liczba_dziel);
+}

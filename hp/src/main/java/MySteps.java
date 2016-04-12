819d9600f8d82fe3d56f4df74c94b9e8cb439d30
wojtasss <wojcieh.rzepecki@gmail.com>
2016-04-12 21:26:09 +0200

    Create jbehave files

---
+++ hp/src/main/java/MySteps.java
@@ -0,0 +1,23 @@
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+
+import java.util.ArrayList;
+
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Named;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+import org.jbehave.core.steps.Steps;
+
+import hashproject.hp.Hash;
+
+public class MySteps extends Steps{
+	
+	private Hash stack = new Hash();
+	
+	
+}
---
+++ hp/src/main/java/RunJBehaveJUnitStoriesTest.java
@@ -0,0 +1,43 @@
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.jbehave.core.configuration.Configuration;
+import org.jbehave.core.configuration.MostUsefulConfiguration;
+import org.jbehave.core.io.LoadFromClasspath;
+import org.jbehave.core.junit.JUnitStories;
+import org.jbehave.core.reporters.Format;
+import org.jbehave.core.reporters.StoryReporterBuilder;
+import org.jbehave.core.steps.InjectableStepsFactory;
+import org.jbehave.core.steps.InstanceStepsFactory;
+
+public class RunJBehaveJUnitStoriesTest extends JUnitStories {
+
+	public RunJBehaveJUnitStoriesTest(){
+		super();
+	}
+	
+	@Override
+	public Configuration configuration() {
+		return new MostUsefulConfiguration()
+		// where to find the stories
+				.useStoryLoader(new LoadFromClasspath())
+				// CONSOLE and TXT reporting
+				.useStoryReporterBuilder(
+						new StoryReporterBuilder().withDefaultFormats()
+								.withFormats(Format.CONSOLE, Format.TXT));
+	}
+
+	// Here we specify the steps classes
+	@Override
+	public InjectableStepsFactory stepsFactory() {
+		// varargs, can have more that one steps classes
+		return new InstanceStepsFactory(configuration(), new MySteps());
+	}
+	
+		@Override
+	protected List<String> storyPaths() {
+		return Arrays.asList("HashStory.story");
+	}
+
+}

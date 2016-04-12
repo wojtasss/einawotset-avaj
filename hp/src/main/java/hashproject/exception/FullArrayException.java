798774ccd87d2290c8a0ab3f227b25fcf481213e
wojtasss <wojcieh.rzepecki@gmail.com>
2016-04-12 21:12:34 +0200

    Create helper exceptions

---
+++ hp/src/main/java/hashproject/exception/FullArrayException.java
@@ -0,0 +1,5 @@
+package hashproject.exception;
+
+public class FullArrayException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+}
---
+++ hp/src/main/java/hashproject/exception/NotFoundException.java
@@ -0,0 +1,5 @@
+package hashproject.exception;
+
+public class NotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 2L;
+}
